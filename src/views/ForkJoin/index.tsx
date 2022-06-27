945e9cc6e1b8650555951469de00da09fe219c80
wuming <3261937664@qq.com>
2022-06-27 18:44:50 +0800

    解决observable实例化过多的隐患

--- src/views/ForkJoin/index.tsx
+++ src/views/ForkJoin/index.tsx
@@ -1,51 +1,35 @@
 import React, { useEffect, useState } from "react";
 
-import { Observable, of, first, map, forkJoin, timer } from "rxjs";
+import { forkJoin, timer } from "rxjs";
+import { useObservable } from "rxjs-hooks";
 
-const observable1 = new Observable((subscriber) => {
-  setTimeout(() => {
-    subscriber.next(111);
-    subscriber.complete();
-  }, 2000);
-});
-const observable2 = new Observable((subscriber) => {
-  setTimeout(() => {
-    subscriber.next(222);
-    subscriber.complete();
-  }, 4000);
-});
-const observable3 = new Observable((subscriber) => {
-  setTimeout(() => {
-    subscriber.next(333);
-    subscriber.complete();
-  }, 5000);
-});
-
-/**
- * 
- * @TODO 解决Observable在组件中实例化次数过多的隐患。
- */ 
 export function ForkJoin() {
-  const [loaded, setLoaded] = useState<boolean>(false);
-  // const [observable1,observable2,observable3] = useObservable();
-  const observableForkJoin = forkJoin([observable1, observable2, observable3]);
+  const value = useObservable(() =>
+    forkJoin([
+      new Promise((res, rej) => {
+        try {
+          setTimeout(() => {
+            res("loaded");
+          }, 5000);
+        } catch (error) {
+          rej(error);
+        }
+      }),
+      timer(2000),
+      timer(1000),
+    ])
+  );
 
-  observableForkJoin.subscribe({
-    next: (value) => {
-      console.log(value);
-    },
-    complete() {
-      setLoaded(true);
-      console.log("执行完了");
-    },
-  });
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
 
   return (
     <div>
       rxjs Study...
       <br /> 页面加载完了吗？
       <br />
-      {loaded ? "加载完了" : "加载中..."}
+      {value ? "加载完了" : "加载中..."}
     </div>
   );
 }
